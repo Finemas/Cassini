43440cc74cf07d97a0ffb23e536e413eae0c60a7
Jan Provazník <proovaznik@gmail.com>
2020-03-17 12:42:29 +0100

    The ninth Standford lecture
    ScrollView

---
+++ Cassini/DemoURLs.swift
@@ -0,0 +1,13 @@
+//
+//  DemoURLs.swift
+//  Cassini
+//
+//  Created by fineas on 17/03/2020.
+//  Copyright © 2020 Jan Provazník. All rights reserved.
+//
+
+import Foundation
+
+struct DemoURLs {
+    static let standford = Bundle.main.url(forResource: "oval", withExtension: "jpg")
+}
---
+++ Cassini/ImageViewController.swift
@@ -0,0 +1,71 @@
+//
+//  ViewController.swift
+//  Cassini
+//
+//  Created by fineas on 17/03/2020.
+//  Copyright © 2020 Jan Provazník. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController, UIScrollViewDelegate {
+
+    var imageURL: URL? {
+        didSet {
+            image = nil
+            if view.window != nil {
+                fetchImage()
+            }
+        }
+    }
+    
+    var image: UIImage? {
+        get {
+            return imageView.image
+        }
+        set {
+            imageView.image = newValue
+            imageView.sizeToFit()
+            scrollView.contentSize = imageView.frame.size
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if imageView.image == nil {
+            fetchImage()
+        }
+    }
+    
+    @IBOutlet weak var scrollView: UIScrollView! {
+        didSet {
+            scrollView.minimumZoomScale = 1/25
+            scrollView.maximumZoomScale = 1
+            scrollView.delegate = self
+            scrollView.addSubview(imageView)
+        }
+    }
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
+    var imageView = UIImageView()
+    
+    private func fetchImage() {
+        if let url = imageURL {
+            let urlContents = try? Data(contentsOf: url)
+            if let imageData = urlContents {
+                image = UIImage(data: imageData)
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if imageURL == nil {
+            imageURL = DemoURLs.standford
+        }
+    }
+}
+
